241762e0f740d64ed4e6ac5f5cf521c2abcef763
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-22 21:22:13 +1000

    Test Sonarr bulk monitoring list responses

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -67,9 +67,10 @@ class ClientTests(unittest.TestCase):
         ])
 
     def test_sonarr_destructive_and_command_contracts(self):
+        monitor_response = [{"id": 1, "monitored": False}, {"id": 2, "monitored": False}]
         sonarr = client(
             SonarrClient,
-            [None, None, None, [], {"id": 201}, {"id": 202}, {"id": 203}, {}],
+            [None, None, None, [], {"id": 201}, {"id": 202}, {"id": 203}, monitor_response],
         )
 
         sonarr.delete_episode_file(8)
@@ -79,7 +80,7 @@ class ClientTests(unittest.TestCase):
         self.assertEqual(sonarr.search_episodes([11, 12])["id"], 201)
         self.assertEqual(sonarr.search_series(3)["id"], 202)
         self.assertEqual(sonarr.rescan_series(3)["id"], 203)
-        sonarr.set_episodes_monitored([1, 2], False)
+        self.assertEqual(sonarr.set_episodes_monitored([1, 2, 1], False), monitor_response)
 
         self.assertEqual(sonarr.http.calls, [
             (("DELETE", "/episodeFile/8"), {}),
@@ -106,7 +107,7 @@ class ClientTests(unittest.TestCase):
                 operation()
         self.assertEqual(radarr.http.calls, [])
         self.assertEqual(sonarr.http.calls, [])
-        sonarr.set_episodes_monitored([], False)
+        self.assertIsNone(sonarr.set_episodes_monitored([], False))
         self.assertEqual(sonarr.http.calls, [])
 
 
